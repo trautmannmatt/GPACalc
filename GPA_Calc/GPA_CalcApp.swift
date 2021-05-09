89d52f5ba3bd5f0cf86990ad24b8d5aa68296196
Matthew Trautmann <matthew@computer.home>
2021-05-09 16:37:30 -0400

    second commit

---
+++ GPA_Calc/GPA_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPA_CalcApp.swift
+//  GPA_Calc
+//
+//  Created by Matthew Trautmann on 5/9/21.
+//
+
+import SwiftUI
+
+@main
+struct GPA_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            GPACalculatorView()
+        }
+    }
+}
